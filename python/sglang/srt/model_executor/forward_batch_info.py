e58ebd45b6c43aaf18c13cbb600554c0d55be0a2
LinyuanLi0046 <liliny163@163.com>
2026-06-15 11:16:40 +0800

    bugfix

--- python/sglang/srt/model_executor/forward_batch_info.py
+++ python/sglang/srt/model_executor/forward_batch_info.py
@@ -490,7 +490,7 @@ class ForwardBatch(ForwardBatchDeepSeekMHAMixin):
 
         num_tokens = len(batch.input_ids) if batch.input_ids is not None else 0
         if enable_num_token_non_padded(model_runner.server_args):
-            ret.num_token_non_padded = torch.tensor(num_tokens, dtype=torch.int32).to(
+            ret.num_token_non_padded = torch.tensor(num_tokens, dtype=torch.int32, pin_memory=_pin).to(
                 device, non_blocking=True
             )
         ret.num_token_non_padded_cpu = num_tokens
